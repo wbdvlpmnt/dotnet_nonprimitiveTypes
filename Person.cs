3e1872c8efe6cc68daa2877d3660a5b818b27d46
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-01-06 10:27:10 -0600

    move the person class

---
+++ Person.cs
@@ -0,0 +1,11 @@
+namespace nonprimitive;
+
+public class Person
+{
+    public string Name = "Jai";
+
+    public void Introduce()
+    {
+        System.Console.WriteLine("Hi, my name is " + Name);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,13 +19,3 @@ class Program
 
     }
 }
-
-public class Person
-{
-    public string Name = "Jai";
-
-    public void Introduce()
-    {
-        System.Console.WriteLine("Hi, my name is " + Name);
-    }
-}
