b98961b6cd349636abd3ee7eceb82a175bafa879
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-01-06 10:16:53 -0600

    Using classes

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Person person = new Person();
+        person.Introduce();
+
+        // var keyword to allocate memory
+        var person1 = new Person();
+        person1.Name = "Jaidev";
+        person1.Introduce();
+
     }
 }
 
+public class Person
+{
+    public string Name = "Jai";
+
+    public void Introduce()
+    {
+        System.Console.WriteLine("Hi, my name is " + Name);
+    }
+}
